a65e79df477d11a4af3569bfffe6be703c5d5a3a
luisccgoncalves <luisccgoncalves@gmail.com>
2018-11-04 18:19:08 +0000

    added intro animation

--- BattleShiPP/BattleShiPP/BattleShiPP.cpp
+++ BattleShiPP/BattleShiPP/BattleShiPP.cpp
@@ -3,17 +3,33 @@
 
 using namespace std;
 
-int main() {
+void printBanner() {
 
-
-	Consola::setTextColor(Consola::AZUL_CLARO);
-	Consola::gotoxy(0, 10);
 	cout << "  ____        _   _   _       _____ _     _ _____  _____  " << endl;
 	cout << " |  _ \\      | | | | | |     / ____| |   (_)  __ \\|  __ \\ " << endl;
 	cout << " | |_) | __ _| |_| |_| | ___| (___ | |__  _| |__) | |__) |" << endl;
 	cout << " |  _ < / _` | __| __| |/ _ \\\\___ \\| '_ \\| |  ___/|  ___/ " << endl;
 	cout << " | |_) | (_| | |_| |_| |  __/____) | | | | | |    | |     " << endl;
 	cout << " |____/ \\__,_|\\__|\\__|_|\\___|_____/|_| |_|_|_|lus |_|lus  " << endl;
+
+}
+
+void intro() {
+
+	for (int i = 14; i >= 0; i--) {
+		Consola::gotoxy(0, i);
+		printBanner();
+		Sleep(80);
+		if (i)Consola::clrscr();
+	}
+}
+
+int main() {
+
+
+	Consola::setTextColor(Consola::AZUL_CLARO);
+
+	intro();
 
 	Consola::getch();
 
